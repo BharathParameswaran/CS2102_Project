2d638a3e103c2832b99ce8d9e4a6fc76cb48e116
Bharath <bharathparam94@gmail.com>
2014-10-24 16:50:15 +0800

    added front end support for filters

--- js/hotelBooking.js
+++ js/hotelBooking.js
@@ -3,13 +3,28 @@
     var link = document.querySelector('link[id=homePage]');
     var content = link.import.querySelector('#home');
     document.body.appendChild(document.importNode(content, true));   
-
+    
     $("#datepicker").datepicker({
-      dateFormat: "dd-mm-yy"
+      dateFormat: "dd-mm-yy",
+      minDate: 1,
+      defaultDate: 1,
+      
     });
+
+    $('#datepicker').on('change', function(){
+      var min = $(this).val();
+      console.log(min);
+      if(min > $( "#datepicker1" ).val())
+        $( "#datepicker1" ).val("");
+    $( "#datepicker1" ).datepicker( "destroy" );
     $("#datepicker1").datepicker({
-      dateFormat: "dd-mm-yy"
+      dateFormat: "dd-mm-yy",
+      minDate: min,
+      defaultDate: min
     });
+
+    });
+
 
     $('#createAcc').click(function() {
 
@@ -17,48 +32,13 @@
     });
 
     $("#getSearchResult").click(function() {
+     var city = $('#searchInput').val();
+     var checkInDate = $('#datepicker').datepicker("getDate");
+     var checkOutDate = $('#datepicker1').datepicker("getDate");
+     var errorCount1=  validateInput(city, checkInDate, checkOutDate);
 
-      var errorString = "Please fill the following fields:</br>";
-      var errorString2 = ""
-      var errorCount1 = 1;
-      var errorCount2 = 1;
 
-      var city = $("#searchInput").val();
-      if (city == "") {
-        errorString += errorCount1++ +".Location / Hotel name </br>";
-        console.log("jsf");
-      }
-
-      var ciDate = $("#datepicker").datepicker("getDate");
-      if (ciDate == null) {
-        errorString += errorCount1++ +".Check-in date </br>";
-      } else {
-        if (ciDate < new Date()) {
-          errorString2 += "Check-in date chosen has already past</br>";
-          errorCount2++;
-        } else
-          var checkInDate = ciDate.getFullYear() + '-' + ('0' + (ciDate.getMonth() + 1)).slice(-2) + '-' + ('0' + ciDate.getDate()).slice(-2);
-      }
-
-      var coDate = $("#datepicker1").datepicker("getDate");
-      if (coDate == null) {
-        errorString += errorCount1++ +".Check-out date.</br>";
-      } else {
-        var checkOutDate = coDate.getFullYear() + '-' + ('0' + (coDate.getMonth() + 1)).slice(-2) + '-' + ('0' + coDate.getDate()).slice(-2);
-        if (checkInDate && (ciDate > coDate)) {
-          errorString2 += "Check-in date must be before check-out date</br>";
-          errorCount2++;
-        }
-      }
-
-      if (errorCount1 != 1 || errorCount2 != 1) {
-        if (errorCount1 == 1)
-          $('.alert-success').children('span').html(errorString2);
-        else
-          $('.alert-success').children('span').html(errorString + errorString2);
-        $('.alert-success').slideDown(500).delay(3000).slideUp(500);
-      }
-      if (errorCount1 == 1 && errorCount2 == 1) {
+      if (errorCount1 == 1) {
 
         $.ajax({
           type: "GET",
@@ -67,10 +47,23 @@
 
           success: function(result) {
             result = JSON.parse(result);
-
-            if (result['status'] == 'ok') {
               console.log(result);
               showHotels();
+                  $("#checkIn").datepicker({
+                    dateFormat: "dd-mm-yy"
+                  });
+                  $("#checkOut").datepicker({
+                    dateFormat: "dd-mm-yy"
+                  });
+
+              $('#search').val(city);
+              $('#checkIn').val(formatDate(checkInDate));
+              $('#checkOut').val(formatDate(checkOutDate));
+              addRangeSelector();
+
+
+            if (result['status'] == 'ok') {
+          
               var hotels = result['answer'];
               var hotelsDiv = $('<ul>').addClass('list-group');
               $.each(hotels, function(index) {
@@ -82,19 +75,54 @@
                   .append($('<p>').addClass('col-md-6').html("Rooms available: " + hotels[index]['emptyRooms'])));
 
                 hotel.append($('<p>').html(hotels[index]['rating'] + "/5"));
-                var listElement = $('<li>').addClass('list-group-item col-md-5 col-md-offset-2').append(hotel);
+                var listElement = $('<li>').addClass('list-group-item').append(hotel);
                 hotelsDiv.append(listElement);
               });
               $('#hotels').append(hotelsDiv);
             } else if (result['status'] == 'empty'){
-               console.log(result);
-               showHotels();
+
                $('#hotels').html("No results found. Try refining your search!");
             }
           }
         });
       }
     });
+
+
+  var validateInput = function(city, ciDate, coDate){
+
+    var errorString = "Please fill the following fields:</br>";
+      var errorString2 = ""
+      var errors= 1;
+    
+      
+      if (city == "") {
+        errorString += errors++ +".Location / Hotel name </br>";
+      }
+
+      if (ciDate == null) {
+        errorString += errors++ +".Check-in date </br>";
+      } else {
+          var checkInDate = formatDate(ciDate);
+      }
+
+      if (coDate == null) {
+        errorString += errors++ +".Check-out date.</br>";
+      } else {
+        var checkOutDate = formatDate(coDate);
+      }
+
+      if (errors != 1) {
+          $('.alert-success').children('span').html(errorString);
+          $('.alert-success').slideDown(500).delay(3000).slideUp(500);
+      }
+
+      return errors;
+  }
+
+  var formatDate = function(date){
+      return date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
+  };
 
   });
 
@@ -104,3 +132,29 @@
     var content = link.import.querySelector('#searchResults');
     document.body.appendChild(document.importNode(content, true)); 
   };
+
+  var addRangeSelector = function(){
+  $( "#amount-slider" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 200, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#amount-slider" ).slider( "values", 0 ) +
+      " - $" + $( "#amount-slider" ).slider( "values", 1 ) );
+
+     $( "#rating-slider" ).slider({
+      range: true,
+      min: 0,
+      max: 5,
+      values: [ 3, 5 ],
+      slide: function( event, ui ) {
+        $( "#rating" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
+    });
+    $( "#rating" ).val( "$" + $( "#rating-slider" ).slider( "values", 0 ) +
+      " - $" + $( "#rating-slider" ).slider( "values", 1 ) );
+};
