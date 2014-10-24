a08f5f84d242ef9e38ba1db09d7abb2dcb140f81
Bharath <bharathparam94@gmail.com>
2014-10-24 13:40:07 +0800

    used html imports

--- js/hotelBooking.js
+++ js/hotelBooking.js
@@ -1,80 +1,106 @@
-$(function() {
+  $(function() {
+
+    var link = document.querySelector('link[id=homePage]');
+    var content = link.import.querySelector('#home');
+    document.body.appendChild(document.importNode(content, true));   
 
     $("#datepicker").datepicker({
-        dateFormat: "dd-mm-yy"
+      dateFormat: "dd-mm-yy"
     });
     $("#datepicker1").datepicker({
-        dateFormat: "dd-mm-yy"
+      dateFormat: "dd-mm-yy"
     });
 
-    $('#createAcc').click(function(){
+    $('#createAcc').click(function() {
 
 
     });
 
     $("#getSearchResult").click(function() {
 
-        var errorString = "Please fill the following fields:</br>";
-        var errorString2 = "Check-in date must be before Check-out date</br>"
-        var errorCount1 = 1;
-        var errorCount2 = 1;
+      var errorString = "Please fill the following fields:</br>";
+      var errorString2 = ""
+      var errorCount1 = 1;
+      var errorCount2 = 1;
 
-        var city = $("#searchInput").val();
-        if (city == "") {
-            errorString += errorCount1++ +".Location / Hotel name </br>";
+      var city = $("#searchInput").val();
+      if (city == "") {
+        errorString += errorCount1++ +".Location / Hotel name </br>";
+        console.log("jsf");
+      }
+
+      var ciDate = $("#datepicker").datepicker("getDate");
+      if (ciDate == null) {
+        errorString += errorCount1++ +".Check-in date </br>";
+      } else {
+        if (ciDate < new Date()) {
+          errorString2 += "Check-in date chosen has already past</br>";
+          errorCount2++;
+        } else
+          var checkInDate = ciDate.getFullYear() + '-' + ('0' + (ciDate.getMonth() + 1)).slice(-2) + '-' + ('0' + ciDate.getDate()).slice(-2);
+      }
+
+      var coDate = $("#datepicker1").datepicker("getDate");
+      if (coDate == null) {
+        errorString += errorCount1++ +".Check-out date.</br>";
+      } else {
+        var checkOutDate = coDate.getFullYear() + '-' + ('0' + (coDate.getMonth() + 1)).slice(-2) + '-' + ('0' + coDate.getDate()).slice(-2);
+        if (checkInDate && (ciDate > coDate)) {
+          errorString2 += "Check-in date must be before check-out date</br>";
+          errorCount2++;
         }
+      }
 
-        var ciDate = $("#datepicker").datepicker("getDate");
-        if (ciDate == null) {
-            errorString += errorCount1++ +".Check-in date </br>";
-        } else {
-            var checkInDate = ciDate.getFullYear() + '-' + ('0' + (ciDate.getMonth() + 1)).slice(-2) + '-' + ('0' + ciDate.getDate()).slice(-2);
-        }
+      if (errorCount1 != 1 || errorCount2 != 1) {
+        if (errorCount1 == 1)
+          $('.alert-success').children('span').html(errorString2);
+        else
+          $('.alert-success').children('span').html(errorString + errorString2);
+        $('.alert-success').slideDown(500).delay(3000).slideUp(500);
+      }
+      if (errorCount1 == 1 && errorCount2 == 1) {
 
-        var coDate = $("#datepicker1").datepicker("getDate");
-        if (coDate == null) {
-            errorString += errorCount1++ +".Check-out date.";
-        } else {
-            var checkOutDate = coDate.getFullYear() + '-' + ('0' + (coDate.getMonth() + 1)).slice(-2) + '-' + ('0' + coDate.getDate()).slice(-2);
-            if(checkInDate && (ciDate > coDate)){
-            errorString += "Check-in date must be before check-out date</br>";
-            errorCount2++;
+        $.ajax({
+          type: "GET",
+          url: "php/getHotels.php",
+          data: "string=" + city + "&checkInDate=" + checkInDate + "&checkOutDate=" + checkOutDate,
+
+          success: function(result) {
+            result = JSON.parse(result);
+
+            if (result['status'] == 'ok') {
+              console.log(result);
+              showHotels();
+              var hotels = result['answer'];
+              var hotelsDiv = $('<ul>').addClass('list-group');
+              $.each(hotels, function(index) {
+
+                var hotel = $('<div>').addClass('list-group-item-heading').attr('id', hotels[index]['id'])
+                  .append($('<a>').html(hotels[index]['name']));
+                hotel.append($('<div>').addClass('row')
+                  .append($('<p>').addClass('col-md-6').html(hotels[index]['street'] + ", " + hotels[index]['country']))
+                  .append($('<p>').addClass('col-md-6').html("Rooms available: " + hotels[index]['emptyRooms'])));
+
+                hotel.append($('<p>').html(hotels[index]['rating'] + "/5"));
+                var listElement = $('<li>').addClass('list-group-item col-md-5 col-md-offset-2').append(hotel);
+                hotelsDiv.append(listElement);
+              });
+              $('#hotels').append(hotelsDiv);
+            } else if (result['status'] == 'empty'){
+               console.log(result);
+               showHotels();
+               $('#hotels').html("No results found. Try refining your search!");
             }
-        }
-
-        if (errorCount1 != 1) {
-
-            $('.alert-success').children('span').html(errorString);
-            $('.alert-success').slideDown(500).delay(2000).slideUp(500);
-        }
-        else if(errorCount2 !=1){
-             $('.alert-success').children('span').html(errorString2);
-            $('.alert-success').slideDown(500).delay(2000).slideUp(500);
-
-        }
-         else {
-
-            $.ajax({
-                type: "GET",
-                url: "php/getHotels.php",
-                data: "string=" + city + "&checkInDate=" + checkInDate + "&checkOutDate=" + checkOutDate,
-
-                success: function(result) {
-                    result = JSON.parse(result);
-
-                    if (result['status'] == 'ok') {
-                        console.log(result);
-                        showHotels();           
-                    } else {
-
-                    }
-                }
-            });
-        }
+          }
+        });
+      }
     });
 
-});
+  });
 
-var showHotels = function(){
-
-};
+  var showHotels = function() {
+    $('#home').hide();
+    var link = document.querySelector('link[id=resultsPage]');
+    var content = link.import.querySelector('#searchResults');
+    document.body.appendChild(document.importNode(content, true)); 
+  };
--- php/getHotels.php
+++ php/getHotels.php
@@ -7,27 +7,40 @@ $city = '%'.$_GET['string'].'%';
 $in = $_GET['checkInDate'];
 $out = $_GET['checkOutDate'];
 
-$stmt = mysqli_prepare($con, "SELECT hname, streetName, rating FROM hotel h WHERE (hname LIKE ? OR streetName LIKE ? 
-	OR country LIKE ?) AND EXISTS (SELECT * FROM room r WHERE r.hid= h.hid AND roomNo NOT IN 
-	(SELECT roomNo FROM booking b WHERE b.hid=h.hid AND ((b.checkInDate <= ? 
-		AND DATE_ADD(b.checkInDate,INTERVAL b.duration DAY) >= ?) 
-		OR (b.checkInDate >= ? AND b.checkInDate <= ?))))");
-mysqli_stmt_bind_param($stmt, 'sssssss', $city, $city, $city, $in, $in, $in, $out);
+$stmt = mysqli_prepare($con, "SELECT T.Hid, hname, streetName, country, rating, t.count FROM hotel h,  
+(SELECT room.hid, COUNT(*) AS count FROM room WHERE roomNo NOT IN 
+(SELECT roomNo FROM booking b WHERE (b.hid = room.hid AND b.roomNo = room.roomNo) AND ((b.checkInDate <= ? AND DATE_ADD(b.checkInDate,INTERVAL b.duration DAY) >= ?) 
+OR (b.checkInDate >= ? AND b.checkInDate <= ?))) 
+GROUP BY hid) 
+AS T WHERE T.hid = h.hid AND t.count >0
+AND (hname LIKE ? OR streetName LIKE ?  OR country LIKE ?)");
+mysqli_stmt_bind_param($stmt, 'sssssss', $in, $in, $in, $out, $city, $city, $city);
 mysqli_stmt_execute($stmt);
-mysqli_stmt_bind_result($stmt, $name, $street, $rating);
+mysqli_stmt_bind_result($stmt, $id, $name, $street,$country, $rating, $count);
 echo $name;
     $result = array();
+    $num_rows =0;
 
     while (mysqli_stmt_fetch($stmt)) {
+        $num_rows ++;
     	$returnedHotel = array();
+    	$returnedHotel['id'] = $id;
     	$returnedHotel['name'] = $name;
     	$returnedHotel['street'] = $street;
+    	$returnedHotel['country'] = $country;
     	$returnedHotel['rating'] = $rating;
+    	$returnedHotel['emptyRooms'] = $count;
     	array_push($result, $returnedHotel);
     }
-    json_encode($result);
+    if($num_rows == 0){
+        $reply['status'] = "empty";
+    }
+    else{
+        json_encode($result);
+        $reply['answer'] = $result;
 
-$reply['answer'] = $result;
+    }
+
 }
 print json_encode($reply);
 ?>
