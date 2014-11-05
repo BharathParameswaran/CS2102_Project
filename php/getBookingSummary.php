d59b453b7045c2c7852b902577885fd98ab88999
celiatio <tiocelia@gmail.com>
2014-11-05 21:14:38 +0800

    getBookingSummary

--- php/getBookingSummary.php
+++ php/getBookingSummary.php
@@ -10,33 +10,44 @@ $checkInDate = $_GET['checkInDate'];
 $checkOutDate = $_GET['checkOutDate'];
 
 
-$stmt = mysqli_prepare($con, "SELECT r.roomNo, r.price, c.cname FROM room r, category c WHERE r.hId=? AND r.cId=? AND r.cId = c.cId AND r.roomNo NOT IN (SELECT roomNo FROM booking b WHERE (b.hid = r.hid AND b.roomNo = r.roomNo) AND ((b.checkInDate <= ? AND DATE_ADD(b.checkInDate,INTERVAL b.duration DAY) >= ?) OR (b.checkInDate >= ? AND b.checkInDate <= ?)))");
+$stmt = mysqli_prepare($con, "SELECT r.roomNo, r.price, c.cname FROM room r, category c WHERE r.hId=? AND r.cId=? AND r.cId = c.cId AND r.roomNo NOT IN (SELECT roomNo FROM booking b WHERE (b.hid = r.hid AND b.roomNo = r.roomNo) AND ((b.checkInDate <= ? AND DATE_ADD(b.checkInDate,INTERVAL b.duration DAY) >=?) OR (b.checkInDate >= ? AND b.checkInDate <= ?)))");
 
 mysqli_stmt_bind_param($stmt, 'ssssss', $hId, $cId, $checkInDate, $checkInDate, $checkInDate, $checkOutDate);
 mysqli_stmt_execute($stmt);
-mysqli_stmt_bind_result($stmt, $cName, $price);
-mysqli_stmt_fetch($stmt);
+mysqli_stmt_bind_result($stmt, $roomNo, $price, $cName);
 
-    if($cName != null ){
-       $reply['status'] = 'Success';
-		$reply['cName'] = $cName;
-		$reply['price'] = $price;
+	$result = array();
+    $num_rows =0;
+
+    while (mysqli_stmt_fetch($stmt)) {
+        $num_rows ++;
+    	$room = array();
+    	$room['roomNo'] = $roomNo;
+    	$room['cName'] = $cName;
+    	$room['price'] = $price;
+    	array_push($result, $room);
     }
-   
+    if($num_rows == 0){
+        $reply['status'] = "Empty";
+    }
+    else{
+    	$reply['status'] = "Success";
+        json_encode($result);
+        $reply['rooms'] = $result;
 
-$hotel = array();
-$hotel['status'] = 'Fail';
+    }
+
+$getHotel = array();
 $statement = mysqli_prepare($con, "select * from hotel WHERE hId = ?");
 mysqli_stmt_bind_param($statement, 's', $hId);
 mysqli_stmt_execute($statement);
 mysqli_stmt_bind_result($statement, $hId, $hName, $unitNo, $street, $country, $postal, $contact, $rating);
 mysqli_stmt_fetch($statement);
 
-	$result = array();
+	$hotel = array();
 
     if($hId != null ){
-    	$hotel = array();
-       
+    	$hotel['hId'] = $hId;
 		$hotel['hName'] = $hName;
 		$hotel['unitNo'] = $unitNo;
 		$hotel['street'] = $street;
@@ -44,9 +55,9 @@ mysqli_stmt_fetch($statement);
 		$hotel['postal'] = $postal;
 		$hotel['contact'] = $contact;
 		$hotel['rating'] = $rating;
-		array_push($result, $hotel);
+		array_push($getHotel, $hotel);
     }
-    $reply['answer'] = $result;
+    $reply['answer'] = $getHotel;
 
    
 
